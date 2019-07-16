ca15a4b5c81841522f220b38691c2a5f5a4556b6
Luke Bonaccorsi <luke@lukeb.co.uk>
2019-07-16 21:10:27 +0100

    Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
 const apiClass = require(path.join(__dirname, `lib`, `api.js`));
 const speechClass = require(path.join(__dirname, `lib`, `speech.js`));
 const broadcastClass = require(path.join(__dirname, `lib`, `broadcast.js`));
@@ -10,4 +10,3 @@ const api = new apiClass();
 const speech = new speechClass();
 const listener = new listenerClass(api, speech);
 const broadcast = new broadcastClass(api, listener);
-
